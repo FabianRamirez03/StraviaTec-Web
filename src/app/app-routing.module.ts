d89109a8c1fd8c7aa494f2515b3b06801cf3ad7f
Fabian <ramirezfabian003@gmail.com>
2020-10-24 17:53:09 -0600

    Pantalla de login bonita

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './Vistas/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Login', component: LoginComponent},
+  {path: '', redirectTo: '/Login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent];
