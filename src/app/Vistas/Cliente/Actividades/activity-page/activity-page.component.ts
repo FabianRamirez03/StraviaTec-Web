9118af3e8d45bd8dbc0dedc02048c76325b93c99
Fabian <ramirezfabian003@gmail.com>
2020-11-01 21:01:17 -0600

    Ventana de mis actividades escalable

--- src/app/Vistas/Cliente/Actividades/activity-page/activity-page.component.ts
+++ src/app/Vistas/Cliente/Actividades/activity-page/activity-page.component.ts
@@ -5,11 +5,28 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './activity-page.component.html',
   styleUrls: ['./activity-page.component.scss']
 })
+
+
 export class ActivityPageComponent implements OnInit {
+  actividades = [
+    {
+      tipo: 'Ciclismo de montaña',
+      esfuerzo: 'Intermedio',
+      nombre: 'Viaje por Chepe',
+      fecha: '11/1/20',
+      hora: '4:20 pm',
+    },
+    {
+      tipo: 'Senderismo',
+      esfuerzo: 'Liviano',
+      nombre: 'Por el monte',
+      fecha: '6/2/18',
+      hora: '9:10 am',
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
