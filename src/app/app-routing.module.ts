6d9ef5a8dbd8f50b8d9dafeb4d7ec6e4ded9009a
Fabian <ramirezfabian003@gmail.com>
2020-11-11 22:53:49 -0600

    Conectado la vista de atleta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './Vistas/login/login.component';
 import {SignInComponent} from './Vistas/sign-in/sign-in.component';
-import { FriendsComponent } from './Vistas/friends/friends.component';
+import {FriendsComponent} from './Vistas/Atleta/friends/friends.component';
 import {HomePageComponent} from './Vistas/Cliente/Home/home-page/home-page.component';
 import {ActivityPageComponent} from './Vistas/Cliente/Actividades/activity-page/activity-page.component';
 import {AddActivtyComponent} from './Vistas/Cliente/Actividades/add-activty/add-activty.component';
 import {ActivityInfoComponent} from './Vistas/Cliente/Actividades/activity-info/activity-info.component';
 import {EditProfileComponent} from './Vistas/edit-profile/edit-profile.component';
-import { FriendsComponent } from './Vistas/Atleta/friends/friends.component';
 import { CarrerasComponent } from './Vistas/Atleta/carreras/carreras.component';
 import { InicioComponent } from './Vistas/Atleta/inicio/inicio.component';
 import { RetosComponent } from './Vistas/Atleta/retos/retos.component';
@@ -17,15 +16,15 @@ import { AdmicarrerasComponent } from './Vistas/Admin/admicarreras/admicarreras.
 import { AdmiretosComponent } from './Vistas/Admin/admiretos/admiretos.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/Login', pathMatch: 'full' },
   {path: 'Login', component: LoginComponent},
   {path: 'SignIn', component: SignInComponent},
-  {path: '', redirectTo: '/Login', pathMatch: 'full' },
   {path: 'friendSearch', component: FriendsComponent},
   {path: 'Home', component: HomePageComponent},
   {path: 'Activity', component: ActivityPageComponent},
   {path: 'newActivity', component: AddActivtyComponent},
   {path: 'activityInfo', component: ActivityInfoComponent},
-  {path: 'editProfile', component: EditProfileComponent}
+  {path: 'editProfile', component: EditProfileComponent},
   {path: 'carreras', component: CarrerasComponent},
   {path: 'inicio', component: InicioComponent},
   {path: 'retos', component: RetosComponent},
@@ -40,4 +39,5 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 export const routingComponents = [LoginComponent, SignInComponent, FriendsComponent, HomePageComponent,
-  ActivityPageComponent, AddActivtyComponent, ActivityInfoComponent, EditProfileComponent];
+  ActivityPageComponent, AddActivtyComponent, ActivityInfoComponent, EditProfileComponent, CarrerasComponent,
+  InicioComponent, RetosComponent, ProgresoComponent, AdmicarrerasComponent, AdmiretosComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,11 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeHeaderComponent } from './Vistas/Cliente/Home/Components/home-header/home-header.component';
 import { MapComponent } from './Vistas/Cliente/Actividades/map/map.component';
-import { FriendsComponent } from './Vistas/Atleta/friends/friends.component';
-import { CarrerasComponent } from './Vistas/Atleta/carreras/carreras.component';
-import { InicioComponent } from './Vistas/Atleta/inicio/inicio.component';
-import { RetosComponent } from './Vistas/Atleta/retos/retos.component';
-import { ProgresoComponent } from './Vistas/Atleta/progreso/progreso.component';
-import { AdmicarrerasComponent } from './Vistas/Admin/admicarreras/admicarreras.component';
-import { AdmiretosComponent } from './Vistas/Admin/admiretos/admiretos.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents,
-    FriendsComponent,
-    CarrerasComponent,
-    InicioComponent,
-    RetosComponent,
-    ProgresoComponent,
-    AdmicarrerasComponent,
-    AdmiretosComponent
     routingComponents,
     HomeHeaderComponent,
     MapComponent,
