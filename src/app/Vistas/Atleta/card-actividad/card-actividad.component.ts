40d85564269bf07abf4864f86fad1102317a1d0d
Wajib Zaglul <wajib10@gmail.com>
2020-11-20 17:56:18 -0600

    [add] Feed de inicio con gpx

--- src/app/Vistas/Atleta/card-actividad/card-actividad.component.ts
+++ src/app/Vistas/Atleta/card-actividad/card-actividad.component.ts
@@ -18,31 +18,6 @@ export class CardActividadComponent implements OnInit {
     this.overwriteXML();
     // this.initMap();
   }
-  initMap(): void {
-    console.log(this.atleta.nombreactividad);
-    const map = L.map('map');
-    this.map = map;
-    const tiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-      attribution: 'Powered by Leaflet Extras',
-      maxZoom: 17
-    });
-    tiles.addTo(this.map);
-    // tslint:disable-next-line:prefer-const
-    let gpxHTml;
-    gpxHTml = new L.GPX(this.xml, {
-      async: true,
-      polyline_options: {
-        color: 'orange',
-        opacity: 0.75,
-        weight: 3,
-        lineCap: 'round'
-      }
-    }).on('loaded', e => {
-      map.fitBounds(e.target.getBounds());
-    });
-    gpxHTml.addTo(this.map);
-
-  }
   private overwriteXML(): void{
     this.xml = 'data:application/xml;charset=UTF-8,' + encodeURIComponent(this.xmlText);
   }
