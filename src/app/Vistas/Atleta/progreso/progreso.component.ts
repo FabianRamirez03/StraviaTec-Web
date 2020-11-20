e6d2a4ac7503d49d41f8db7767a6f04c69faa14e
Mariana Vargas Ramirez <marianavargas11209@gmail.com>
2020-11-20 07:30:42 -0600

    [edit] retos

--- src/app/Vistas/Atleta/progreso/progreso.component.ts
+++ src/app/Vistas/Atleta/progreso/progreso.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Router} from '@angular/router';
+import {MessengerService} from '../../../MessengerService';
 
 @Component({
   selector: 'app-progreso',
@@ -9,8 +10,12 @@ import {Router} from '@angular/router';
 })
 export class ProgresoComponent implements OnInit {
   retos: any;
-  constructor(public httpService: HttpClient, private router: Router) {
-    this.retos = ['Mariana', 'Julio', 'Julio', 'Julio', 'Julio', 'Julio', 'Julio'];
+  atleta: any;
+  eventos: any;
+  constructor(public httpService: HttpClient, private router: Router, private messengerService: MessengerService) {
+    this.messengerService.message.subscribe(value => {this.atleta = value; });
+    this.httpService.post('http://localhost/APIStraviaTec/Retos/retosDisponibles', { primernombre: this.atleta.idusuario}).subscribe(
+      (resp: HttpResponse<any>) => { this.retos = resp; console.log(resp); });
   }
   misRetos(): void{}
   ngOnInit(): void {
--- src/app/Vistas/Atleta/retos/retos.component.ts
+++ src/app/Vistas/Atleta/retos/retos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Router} from '@angular/router';
+import {MessengerService} from '../../../MessengerService';
 
 @Component({
   selector: 'app-retos',
@@ -9,12 +10,21 @@ import {Router} from '@angular/router';
 })
 export class RetosComponent implements OnInit {
   eventos: any;
-  constructor(public httpService: HttpClient, private router: Router) {
-    this.eventos = ['Mariana', 'Julio', 'Julio']; }
+  atleta: any;
+  mensaje: any;
+  constructor(public httpService: HttpClient, private router: Router, private messengerService: MessengerService) {
+    this.messengerService.message.subscribe(value => {this.atleta = value; });
+    this.httpService.post('http://localhost/APIStraviaTec/Retos/retosDisponibles', { primernombre: this.atleta.idusuario}).subscribe(
+      (resp: HttpResponse<any>) => { this.eventos = resp; console.log(resp); });
+  }
 
   ngOnInit(): void {
   }
-  suscribirse(): void{}
+  suscribirse(reto: any): void{
+    this.httpService.post('http://localhost/APIStraviaTec/Retos/addUser',
+      { Iddeportista: this.atleta.idusuario, Idreto: reto.Idreto}).subscribe(
+      (resp: HttpResponse<any>) => { this.mensaje = resp; console.log(resp); });
+  }
   retos(): void{}
 
 }
